f301c1b0f4862660e91b49dd6d3454217235c9db
happy_shrimp <89066728+shrimpstack@users.noreply.github.com>
2026-04-05 09:36:24 +0800

    時機篩選、參照效果的顯示方式

--- main.js
+++ main.js
@@ -22,17 +22,25 @@ function 生成搜尋選項() {
     opt.checked = true;
     opt.addEventListener("change", 更新搜尋);
   });
+  [...時機set].forEach(時機名 => {
+    let opt = new_el("input", {type: "checkbox", name: "時機", value: 時機名});
+    new_el_to_el(時機, "label", [ opt, new_el("span", 時機名) ]);
+    opt.checked = true;
+    opt.addEventListener("change", 更新搜尋);
+  });
 }
 function 更新搜尋() {
   let 目標症候群 = [...find_all(`[name="症候群"]:checked`)].map(opt => opt.value);
   let 目標類型 = [...find_all(`[name="異能類型"]:checked`)].map(opt => opt.value);
   let 目標參照 = [...find_all(`[name="參照"]:checked`)].map(opt => opt.value);
+  let 目標時機 = [...find_all(`[name="時機"]:checked`)].map(opt => opt.value);
   [...列表區.children].slice(1).forEach(資料 => {
     let 顯示 = true;
     if(!目標症候群.includes(資料.data.症候群)) 顯示 = false;
     if(!目標類型.includes(資料.data.異能類型)) 顯示 = false;
     let 有資料參照 = 資料.data.參照arr.find(資料參照 => 目標參照.includes(資料參照));
-    if(!有資料參照) 顯示 = false;
+    let 有符合時機 = 資料.data.時機arr.find(時機名 => 目標時機.includes(時機名));
+    if(!有資料參照 || !有符合時機) 顯示 = false;
     資料.style.display = 顯示 ? "" : "none";
   });
 }
@@ -40,6 +48,7 @@ function 更新搜尋() {
 const 症候群set = new Set();
 const 參照set = new Set();
 const 類型set = new Set();
+const 時機set = new Set();
 
 async function 生成顯示() {
   let res = await post("取得全資料");
@@ -65,6 +74,7 @@ function show_row(sheet_name, row) {
   症候群set.add(el_data.症候群);
   類型set.add(el_data.異能類型);
   el_data.參照arr.forEach(參照名 => 參照set.add(參照名));
+  el_data.時機arr.forEach(時機 => 時機set.add(時機));
 
   /* 顯示el */
   let row_el = new_el_to_el(列表區, "div.資料", [
@@ -97,18 +107,24 @@ function 資料處理(sheet_name, row) {
     let 參照名 = str.split("P")[0];
     el_data.參照arr.push(參照名);
   });
+  el_data.時機arr = [];
+  row.時機 = row.時機.replace(/回應動作/g, "反應動作");
+  row.時機 = row.時機.replace(/準備階段/g, "設置階段");
+  row.時機.split("/").forEach(str => {
+    el_data.時機arr.push(str);
+  });
 
   /* 症候群 */
   show_data.主類別 = sheet_name;
   if(!["一般", "自動取得", "D露易絲"].includes(row.異能類型)) {
-    show_data.副類別 = `(${row.異能類型})`;
+    show_data.副類別 = row.異能類型;
   }
   else show_data.副類別 = "";
 
   /* 名稱 */
   let 名稱 = row.名稱.replace(/\n/, "").replace(/（/g, "(").replace(/）/g, ")");
   show_data.中文 = 名稱.split("(")[0];
-  show_data.日文 = row.日文 || 名稱.replace(/^[^\(]*/, "");
+  show_data.日文 = row.日文 || 名稱.replace(/^[^\(]*|\(|\)/g, "");
 
   /* 限制 */
   if(typeof row.限制 == "number") row.限制 = row.限制 * 100 + "%";
@@ -116,10 +132,13 @@ function 資料處理(sheet_name, row) {
 
   /* 資訊列 */
   show_data.資訊列 = [];
+  let 需要參照效果 = false;
   ["時機", "技能", "難易度", "對象", "射程", "侵蝕值", "限制", "次數"].forEach(key => {
     if(!row[key] || row[key] == "－") return;
-    show_data.資訊列.push(`${key}：${row[key]}`);
+    if(row[key] != "參照效果") show_data.資訊列.push(`${key}：${row[key]}`);
+    else 需要參照效果 = true;
   });
+  if(需要參照效果) show_data.資訊列.push("參照效果");
 
   /* 其他 */
   show_data.最大等級 = row.最大等級;
